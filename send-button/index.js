ac1cb80ff5b6893a6d7491dbe6bb9737a1ec8968
clever-king-402 <122508802+clever-king-402@users.noreply.github.com>
2023-02-01 22:11:27 +0545

    Add files via upload

---
+++ send-button/index.js
@@ -0,0 +1,11 @@
+
+
+const circle = document.getElementById('circle');
+circle.addEventListener('click',doThe)
+
+
+function doThe(){
+    const circlep = document.getElementById('circle')
+    circlep.classList.toggle('future')
+
+}
