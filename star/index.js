112ef336e8408e2819bb3ce244ad1af2420fc3db
clever-king-402 <122508802+clever-king-402@users.noreply.github.com>
2023-02-03 12:05:30 +0545

    Add files via upload

---
+++ star/index.js
@@ -0,0 +1,35 @@
+const box = document.getElementById('box')
+var select;
+box.addEventListener('mouseover',function(event){
+    targetId = event.target
+    if(targetId.className ==='star'){
+        for(var i=1;i<=Number(targetId.id);i++){
+            document.getElementById(String(i)).style.color='red'
+        }
+    }
+});
+box.addEventListener("mouseout",function(event){
+    targetId = event.target
+    if(targetId.className ==='star'){
+
+        if(select===undefined){for(var i=1;i<=Number(targetId.id);i++){
+            document.getElementById(String(i)).style.color=''
+        }}
+        else{
+            for(var i=select+1;i<=Number(targetId.id);i++){
+            document.getElementById(String(i)).style.color=''
+        }
+        }
+    }
+});
+
+box.addEventListener('click',function(event){
+    targetId = event.target
+    select = Number(targetId.id)
+    for(var i=1;i<=select;i++){
+            document.getElementById(String(i)).style.color='red'
+        }
+    for(var i=select+1;i<=5;i++){
+            document.getElementById(String(i)).style.color='grey'
+        }
+})
